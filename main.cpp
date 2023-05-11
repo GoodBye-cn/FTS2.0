30451a4dca8e8fca75cdcc5c673fc771c3600344
Goodbye <guwei9911@outlook.com>
2023-05-11 14:54:47 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "Reactor.h"
 
 int main(int, char**) {
+    Reactor reactor;
+    reactor.start();
     std::cout << "Hello, world!\n";
 }
--- src/Acceptor.cc
+++ src/Acceptor.cc
@@ -1,5 +1,6 @@
 #include "Acceptor.h"
 #include "Handler.h"
+#include "Reactor.h"
 
 Acceptor::Acceptor(/* args */) {}
 
@@ -9,6 +10,7 @@ void Acceptor::accept_conn(struct evconnlistener* listener,
     evutil_socket_t fd, struct sockaddr* address, int socklen,
     void* ctx) {
     Reactor* reactor = (Reactor*)ctx;
+    reactor->remove_handler();
     /* 此时已经接收到了文件描述符，调用Handler处理读写事件 */
     event_base* base = evconnlistener_get_base(listener);
     Handler* handler = new Handler();
